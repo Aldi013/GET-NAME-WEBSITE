f0c0b90922859e6358664855ec687442d25fcd66
Aldi013 <akunsingpertama5@gmail.com>
2025-08-09 14:32:09 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,13 @@ const tg = window.Telegram.WebApp;
 document.getElementById("cekNama").addEventListener("click", function () {
     // Ambil data user
     let userData = tg.initDataUnsafe.user;
+
+    // Nama
     let firstName = (userData && userData.first_name) || "tidak ada nama";
-    
-    document.getElementById("hasil").innerText = `Nama pengguna: ${firstName}`;
+
+    // ID
+    let userId = (userData && userData.id) || "tidak ada id";
+
+    // Tampilkan
+    document.getElementById("hasil").innerText = `Nama: ${firstName} | ID: ${userId}`;
 });
